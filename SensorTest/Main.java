97368a7a4fd46cb09e3e3d5e69307196dfad5d63
Manuel Bottazzi <manuel.bottazzi@studio.unibo.it>
2018-12-11 10:51:58 +0100

    added physical motor controller

--- SensorTest/Main.java
+++ SensorTest/Main.java
@@ -13,50 +13,110 @@ public class Main
         IRModule dx = new IRModule(RaspiPin.GPIO_15, "Dx", motorController);
         IRModule sx = new IRModule(RaspiPin.GPIO_16, "Sx", motorController);
 
+        Motor rightWheel = new Motor("right");
+        Motor leftWheel = new Motor("left");
+
         SerialReader reader = new SerialReader();
         Button button = new Button();
-
-        // motor.start();
         
         while(true){
 
-            System.out.println("Waiting for a client !");
+            System.out.println("Wheel test 1 !");
 
-            while(! button.isPressed()) { Thread.sleep(100); }
+            System.out.println("Going Forward ! ");
 
-            System.out.println("A client is arrived !");
-            button.setPressed(false);
-            System.out.println("Going to the client !");
+            rightWheel.forward();
+            leftWheel.forward();
 
-            Thread.sleep(15000);
+            Thread.sleep(5000);
 
-            System.out.println("At table, waiting for orders !");
+            System.out.println("stop ! ");
 
-            while(! button.isPressed()) { Thread.sleep(100); }
+            rightWheel.brake();
+            leftWheel.brake();
 
-            System.out.println("pressed button ! The client has choose his tea");
-            button.setPressed(false);
-            System.out.println("Going to bar !");
+            Thread.sleep(3000);
 
-            Thread.sleep(15000);
+            System.out.println("Going Backward ! ");
 
-            System.out.println("At bar, waiting for preparation !");
+            rightWheel.backward();
+            leftWheel.backward();
+            Thread.sleep(5000);
 
-            while(!button.isPressed()) { Thread.sleep(100); }
+            System.out.println("turning left ! ");
 
-            System.out.println("pressed button ! they give me some tea ");
-            button.setPressed(false);
-            System.out.println("Going back to client !");
+            rightWheel.brake();
+            leftWheel.brake();
+            Thread.sleep(1000);
+            rightWheel.forward();
+            leftWheel.backward();
+            Thread.sleep(5000);
 
-            Thread.sleep(15000);
+            System.out.println("turning right ! ");
 
-            System.out.println("At client, waiting for him to retrieve his tea !");
+            rightWheel.brake();
+            leftWheel.brake();
+            Thread.sleep(1000);
+            rightWheel.backward();
+            leftWheel.forward();
+            Thread.sleep(5000);
 
-            while(!button.isPressed()) { Thread.sleep(100); }
+            System.out.println("going left ! ");
 
-            System.out.println("pressed button ! the client receive the tea");
-            button.setPressed(false);
-            System.out.println("Coming back to start !");
+            rightWheel.brake();
+            leftWheel.brake();
+            Thread.sleep(1000);
+            rightWheel.forward();
+            leftWheel.brake();
+            Thread.sleep(5000);
+
+
+            System.out.println("going right ! ");
+
+            rightWheel.brake();
+            leftWheel.brake();
+            Thread.sleep(1000);
+            rightWheel.brake();
+            leftWheel.forward();
+            Thread.sleep(5000);
+
+            // System.out.println("Waiting for a client !");
+
+            // while(! button.isPressed()) { Thread.sleep(100); }
+
+            // System.out.println("A client is arrived !");
+            // button.setPressed(false);
+            // System.out.println("Going to the client !");
+
+            // Thread.sleep(15000);
+
+            // System.out.println("At table, waiting for orders !");
+
+            // while(! button.isPressed()) { Thread.sleep(100); }
+
+            // System.out.println("pressed button ! The client has choose his tea");
+            // button.setPressed(false);
+            // System.out.println("Going to bar !");
+
+            // Thread.sleep(15000);
+
+            // System.out.println("At bar, waiting for preparation !");
+
+            // while(!button.isPressed()) { Thread.sleep(100); }
+
+            // System.out.println("pressed button ! they give me some tea ");
+            // button.setPressed(false);
+            // System.out.println("Going back to client !");
+
+            // Thread.sleep(15000);
+
+            // System.out.println("At client, waiting for him to retrieve his tea !");
+
+            // while(!button.isPressed()) { Thread.sleep(100); }
+
+            // System.out.println("pressed button ! the client receive the tea");
+            // button.setPressed(false);
+            // System.out.println("Coming back to start !");
         }
         
     }
